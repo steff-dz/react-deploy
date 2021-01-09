b8cb356d428f7071b468213a69727b3eb2baf896
Steff <stefaniemarie@gmail.com>
2021-01-09 16:08:31 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
 		<div className="App">
 			<h1>My React App</h1>
 			<h2>Author: Steff </h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sequi nam doloremque vel tenetur non
+				veritatis quasi quod illo! Accusamus quis debitis ad architecto fugiat ducimus quos quae, nemo nobis?
+				Excepturi recusandae ab numquam dolor saepe fugiat omnis tenetur magnam architecto consectetur labore
+				necessitatibus reprehenderit esse pariatur officiis sapiente ipsum error eveniet, odit quasi quam, natus
+				est in doloribus? Saepe. Molestias quia, non earum quo neque similique minima odit aliquid illo est
+				dolore voluptatibus voluptatum sed quidem excepturi quas inventore ipsum qui corrupti libero itaque eum.
+				Cupiditate minima consequatur ipsa!
+			</p>
 		</div>
 	);
 }
